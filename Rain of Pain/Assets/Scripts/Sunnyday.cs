b3f95c130b5f637283bebbc51a05f4f854ffd856
Yerwts <104331179+Yerwts@users.noreply.github.com>
2023-01-20 15:39:41 -0500

    ok

--- Rain of Pain/Assets/Scripts/Sunnyday.cs
+++ Rain of Pain/Assets/Scripts/Sunnyday.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mr.Sun : MonoBehaviour
+public class Sunnyday: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
